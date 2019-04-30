1e043ab457b2b5f5ea279f40fc146015af4875e3
Jens Åström <jens.astrom@nina.no>
2019-04-30 09:14:25 +0200

    fixed bug that plotted flower cover as a species

--- R/surveyBarplot.R
+++ R/surveyBarplot.R
@@ -19,7 +19,7 @@ surveyBarplot <- function(input,
 ){
 
   agg <- input %>%
-    select(-c(Flate, Transekt, Habitattype, Dato, Year, Periode)) %>%
+    select(-c(Flate, Transekt, Habitattype, Dato, Year, Periode, Blomsterdekke)) %>%
     group_by(Region) %>%
     summarize_all(list(~sum(., na.rm = T))) %>%
     select_if(function(col) is.character(col) || sum(col) > 0) %>%
