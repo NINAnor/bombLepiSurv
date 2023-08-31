a063d46ea8476c9ba8890a6c960810d08a98bc20
Jens Åström <jens.astrom@nina.no>
2023-08-31 15:15:37 +0200

    ggplot version

--- R/plotArt.R
+++ R/plotArt.R
@@ -6,98 +6,115 @@
 #'
 #' @param obs observed community (Its species names will be used)
 #' @param exp expected community
+#' @param addZeroes Add non-observed species, boolean
+#' @param addLegend Add legend, boolean
 #'
 #'
 #'
 #' @export
 
-##This is old and pretty ugly. Should be replaced with a ggplot version.
 
 plotArt <- function(obs,
                     exp,
+                    addZeroes = FALSE,
+                    addLegend = FALSE,
                     ...) {
 
   sum.obs <- sort(colSums(obs[, 4:length(obs)] > 0), decreasing = T)
-  sum.obs <- sum.obs[sum.obs > 0] ## Loose the zeroes
+
+  if(!addZeroes){
+    sum.obs <- sum.obs[sum.obs > 0]
+  }
+
   perc.obs <- sum.obs / nrow(obs)
 
-  cols <- exp$amount[match(names(perc.obs), exp$species)]
-  cols[is.na(cols)] <- "black"
-  cols[cols == ""] <- "black"
-  cols[cols == "g"] <- "grey"
-  cols[cols == "s"] <- "red"
-  cols[cols == "m"] <- "blue"
-  cols[cols == "v"] <- "green"
+  colors <- tibble(amount = c(NA,
+                           "s",
+                           "m",
+                           "v",
+                           "g"),
+                   color = c("black",
+                              "#E57200",
+                              "#008C95",
+                              "#7A9A01",
+                              "grey")
+  )
 
-  x.ticks <- seq(0.15, 1, 0.05)
+  exp <- exp %>%
+    left_join(colors,
+              by = c("amount" = "amount")) %>%
+    mutate(amount = ifelse(is.na(amount), "Ikke forv.", amount),
+           amount = ifelse(amount == "s", "Sjelden", amount),
+           amount = ifelse(amount == "m", "Middels v.", amount),
+           amount = ifelse(amount == "v", "Vanlig", amount),
+           amount = ifelse(amount == "g", "Gjest", amount)
+    )
 
-  max <- min(x.ticks[x.ticks > max(perc.obs)])
+  scale_fill_bombLepi <- function(){
+    scale_fill_manual(values = c("Ikke forv." = "black",
+                                 "Sjelden" = "#E57200",
+                                 "Middels v." = "#008C95",
+                                 "Vanlig" = "#7A9A01",
+                                 "Gjest" = "grey"),
+                      name = "")
+  }
 
-  par(mar = c(5, 12, 4, 2))
-  barplot(
-    rep(0, length(perc.obs)),
-    horiz = T,
-    xlim = c(0, max),
-    axes = F,
-    col = cols,
-    plot = T,
-    xlab = "",
-    ylab = "",
-    ...
-  )
-  axis(1, seq(0, max, 0.05), labels = seq(0, max * 100, 5))
-  lim <- par("usr")
-  rect(
-    0,
-    0,
-    0.01,
-    lim[4],
-    col = rgb(255, 0, 0, alpha = 100, maxColorValue = 255),
-    border = NA
-  )
-  rect(
-    0.01,
-    0,
-    0.05,
-    lim[4],
-    col = rgb(0, 0, 255, alpha = 100, maxColorValue = 255),
-    border = NA
-  )
-  rect(
-    0.05,
-    0,
-    lim[2],
-    lim[4],
-    col = rgb(0, 255, 0, alpha = 100, maxColorValue = 255),
-    border = NA
-  )
-  barplot(
-    perc.obs,
-    horiz = T,
-    las = 1,
-    xlim = c(0, max),
-    axes = F,
-    col = cols,
-    add = T,
-    ...
-  )
-  #abline(v=c(0.01,0.05),lty=2,col="black",lwd=1.5)
-  lines(
-    x = c(0.01, 0.01),
-    y = c(0, lim[4]),
-    lty = 2,
-    col = "black",
-    lwd = 1.5
-  )
-  lines(
-    x = c(0.05, 0.05),
-    y = c(0, lim[4]),
-    lty = 2,
-    col = "black",
-    lwd = 1.5
-  )
-  mtext("Prosentvis transektforekomst",
-        side = 1,
-        line = 3)
+
+
+  plot_data <- tibble(species_latin = names(perc.obs),
+                      perc_obs = perc.obs) %>%
+    left_join(exp,
+              by = c("species_latin" = "species_latin")) %>%
+    mutate(perc_obs = perc_obs * 100) %>%
+    select(species_latin,
+           perc_obs,
+           exp = amount,
+           color) %>%
+    mutate(xaxis_col = glue::glue("<i style='color:{color}'>{species_latin}</i>")) %>%
+    arrange(perc.obs)
+
+ p <- ggplot2::ggplot(plot_data,
+                      aes(y = perc_obs,
+                          x = reorder(xaxis_col, desc(perc_obs)),
+                          fill = exp)) +
+   coord_flip() +
+   ggplot2::theme_classic() +
+   ggplot2::theme(axis.text.y = ggtext::element_markdown()) +
+   ggplot2::xlab("") +
+   ggplot2::ylab("Prosentvis transektforekomst") +
+   geom_blank()
+
+  p <- p +
+    annotate("rect",
+             xmin = 0.5,
+             xmax = nrow(plot_data) + 0.5,
+             ymin = 0,
+             ymax = 1,
+             fill = NinaR::addAlpha("#E5720080", 0.5)) +
+    annotate("rect",
+             xmin = 0.5,
+             xmax = nrow(plot_data) + 0.5,
+             ymin = 1,
+             ymax = 5,
+             fill = NinaR::addAlpha("#0000FF64", 0.5)) +
+    annotate("rect",
+           xmin = 0.5,
+           xmax = nrow(plot_data) + 0.5,
+           ymin = 5,
+           ymax = max(plot_data$perc_obs),
+           fill = NinaR::addAlpha("#00FF0064", 0.5))
+
+    p <-  p +
+      ggplot2::geom_bar(stat = "identity") +
+      scale_fill_bombLepi()
+
+    if(!addLegend){
+      p <- p +
+        theme(legend.position = "none")
+
+    }
+
+
+  return(p)
 
 }
