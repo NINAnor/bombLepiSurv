f2014b0e84980a59a3afe3d55b1d3e7cb07924cd
Jens Åström <jens.astrom@nina.no>
2023-09-01 11:49:10 +0200

    added map with region ost

---
+++ R/get_map.R
@@ -0,0 +1,64 @@
+#' get_map
+#' Fetch a terrestrial map from the database
+#'
+#' @param region_subset optional region(s) to subset
+#'
+#' @return an sf object with fylke name and multipolygon geometry
+#' @export
+#'
+#' @examples
+#'
+#' \dontrun{
+#'
+#' map_from_db <- get_map(region_subset = c("Ost", "Sørlandet"))
+#'
+#' }
+#'
+get_map <- function(region_subset = NULL){
+
+  norway_terr <- sf::read_sf(con,
+                             layer = DBI::Id(schema = "backgrounds", table = "norway_terrestrial")) %>%
+    select(fylke = navn)
+
+
+  region_def <- tibble(region = c("Tr\u00f8ndelag",
+                                  "\u00d8stlandet",
+                                  "\u00d8stlandet",
+                                  "\u00d8stlandet",
+                                  "\u00d8stlandet",
+                                  "S\u00f8rlandet",
+                                  "S\u00f8rlandet",
+                                  "Vestlandet",
+                                  "Vestlandet",
+                                  "Nord-Norge",
+                                  "Nord-Norge",
+                                  "Ost",
+                                  "Ost"),
+                       fylke = c("Tr\u00f8ndelag",
+                                 "Innlandet",
+                                 "Oslo",
+                                 "Vestfold og Telemark",
+                                 "Viken",
+                                 "Rogaland",
+                                 "Agder",
+                                 "Vestland",
+                                 "M\u00f8re og Romsdal",
+                                 "Troms og Finnmark",
+                                 "Nordland",
+                                 "Vestfold",
+                                 "Østfold"))
+
+  norway_terr <- norway_terr %>%
+    left_join(region_def,
+              by = c("fylke" = "fylke"))
+
+  if(!is.null(region_subset)){
+
+    norway_terr <- norway_terr %>%
+      filter(region %in% region_subset)
+
+  }
+
+  return(norway_terr)
+
+}
